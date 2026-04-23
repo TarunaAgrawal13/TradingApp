45ab77637c410482feefb36416a2740f965a26dd
TarunaAgrawal13 <agrawaltaruna13@gmail.com>
2026-04-23 18:42:32 +0530

    baseUrl updated of dashboard to render backend url

--- dashboard/src/config/index.jsx
+++ dashboard/src/config/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3002";
+// export const BASE_URL = "http://localhost:3002";
+export const BASE_URL = "https://tradingapp-mno8.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
